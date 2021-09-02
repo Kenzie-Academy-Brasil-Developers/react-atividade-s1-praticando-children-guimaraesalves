d0f6de602fd937a244e094db3a4200589b815c4d
Mateus Alves <guimaraesalves45@gmail.com>
2021-09-02 15:38:57 -0300

    Commit correcao 2

--- react-atividade-s1-praticando-children-guimaraesalves/src/App.js
+++ react-atividade-s1-praticando-children-guimaraesalves/src/App.js
@@ -1,43 +1,38 @@
 import "./App.css";
 import DatesCards from "./components/CenteredCard";
-//import CenteredCard from "./components/CenteredCard";
+import CenteredCard from "./components/CenteredCard";
 
 function App() {
-  const cookieNames = { a: "Maizena", b: "Cream Cracker", c: "Polvilho" };
-  const cookieCalorias = { a: "19,40kcal", b: "125Kcal", c: "8,88Kcal" };
-  const cookieCarbo = { a: "3,14g", b: "21g", c: "1,68g" };
-  const cookieProt = { a: "0,29g", b: "2,50g", c: "0,06g" };
-  const cookieFibras = { a: "0,16g", b: "0,17g", c: "0,00g" };
-  const cookieSodio = { a: "20,97mg", b: "35,00mg", c: "0,00g" };
-
   return (
     <div className="App">
       <div classNames="App-header">
         <h1>🍪 Tabela Nutricional dos Biscoitos 🍪</h1>
-        <DatesCards
-          nome={cookieNames.a}
-          vlrEner={cookieCalorias.a}
-          carb={cookieCarbo.a}
-          prot={cookieProt.a}
-          fibras={cookieFibras.a}
-          sodio={cookieSodio.a}
-        ></DatesCards>
-        <DatesCards
-          nome={cookieNames.b}
-          vlrEner={cookieCalorias.b}
-          carb={cookieCarbo.b}
-          prot={cookieProt.b}
-          fibras={cookieFibras.b}
-          sodio={cookieSodio.b}
-        ></DatesCards>
-        <DatesCards
-          nome={cookieNames.c}
-          vlrEner={cookieCalorias.c}
-          carb={cookieCarbo.c}
-          prot={cookieProt.c}
-          fibras={cookieFibras.c}
-          sodio={cookieSodio.c}
-        ></DatesCards>
+        <CenteredCard>
+          <h3>Maizena</h3>
+          <p>Valor energético: 19,40kcal</p>
+          <p>Carboidratos: 3,14g</p>
+          <p>Proteínas: 0,29g</p>
+          <p>Fibra alimentar: 0,16g </p>
+          <p>Sódio: 20,97mg </p>
+        </CenteredCard>
+
+        <CenteredCard>
+          <h3>Cream Cracker</h3>
+          <p>Valor energético: 125Kcal</p>
+          <p>Carboidratos: 21g</p>
+          <p>Proteínas: 2,50g</p>
+          <p>Fibra alimentar: 0,17g </p>
+          <p>Sódio: 35,00mg </p>
+        </CenteredCard>
+
+        <CenteredCard>
+          <h3>Polvilho</h3>
+          <p>Valor energético: 8,88Kcal</p>
+          <p>Carboidratos: 1,68g</p>
+          <p>Proteínas: 0,06g</p>
+          <p>Fibra alimentar: 0,00g</p>
+          <p>Sódio: 0,00g</p>
+        </CenteredCard>
       </div>
     </div>
   );
--- react-atividade-s1-praticando-children-guimaraesalves/src/components/CenteredCard.jsx
+++ react-atividade-s1-praticando-children-guimaraesalves/src/components/CenteredCard.jsx
@@ -1,42 +1,11 @@
 import "./styles.css";
 
-/*function CenteredCard(props) {
+function CenteredCard(children) {
   return (
-    <div>
-      <div className="FatherClass">
-        <span className="ChildreClass">
-          <h3>Biscoito: {props.nome}</h3>
-          <p>Valor energético: {props.vlrEner}</p>
-          <p>Carboidratos: {props.carb}</p>
-          <p>Proteínas: {props.prot}</p>
-          <p>Fibra alimentar: {props.fibras}</p>
-          <p>Sódio: {props.sodio}</p>
-        </span>
-      </div>
+    <div className="FatherClass">
+      <span className="ChildreClass">{children}</span>
     </div>
   );
 }
 
 export default CenteredCard;
-*/
-
-function CenteredCard(props) {
-  return <div className="FatherClass">{props.children}</div>;
-}
-
-function DatesCards() {
-  return (
-    <CenteredCard className="ChildrenClass">
-      <span className="ChildreClass">
-        <h3>Biscoito: </h3>
-        <p>Valor energético: </p>
-        <p>Carboidratos: </p>
-        <p>Proteínas: </p>
-        <p>Fibra alimentar: </p>
-        <p>Sódio: </p>
-      </span>
-    </CenteredCard>
-  );
-}
-
-export default DatesCards;
